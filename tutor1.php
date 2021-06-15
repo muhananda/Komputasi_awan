9827f3ce6fbcb8796f8a0bd53003e0579cfe3dc4
muhananda <76613912+muhananda@users.noreply.github.com>
2021-06-15 22:13:23 +0800

    Add files via upload

---
+++ tutor1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Php perkalian pembagian</title>
+</head>
+<body>
+<h1>Belajar Dasar PHP</h1>
+<?php
+/*
+Perkalian di php 10 dikali 4 hasil nya? 
+*/
+$a = 10; // variable a 
+$b = 4; // variable b
+$c = $a * $b; // disini adalah proses nya
+echo "Hasil dari Tambahan $a * $b = $c"; // disini outputnya
+echo "</br>"; // membuat baris baru. sudah belajar html kan? tau dong berarti.
+echo "=======================";
+echo "</br>";
+/*
+Penmbagian di php 8 dibagi 4 hasil nya? 
+*/
+$x = 8; // variable a 
+$y = 4; // variable b 
+$z = $x / $y; // disini adalah proses nya
+echo "Hasil dari Pengurangan $x : $y = $z"; // disini outputnya
+?>
+</body>
+</html>
